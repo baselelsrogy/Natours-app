5e675ce37477b71028e68642f208b33f5a4f9c7f
baselelsrogy <baselhany88@gmail.com>
2025-09-14 16:23:09 +0300

    starting our API

--- app.js
+++ app.js
@@ -4,15 +4,87 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+// Handling GET request
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status:"success",
+    status: 'success',
     reaults: tours.length,
     data: {
       tours: tours,
     },
+  });
+});
+
+// GET tour by id
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id;
+  const tour = tours[id];
+  // if no tour
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// Handling POST request => CREATE
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+});
+
+// Handling PATCH Request => update
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updata tour here>',
+    },
+  });
+});
+
+// Handling DELETE Request
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
