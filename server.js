a9eda0ed9875042cee0c91cf02e4e2bb73be2818
baselelsrogy <baselhany88@gmail.com>
2025-09-16 01:38:25 +0300

    environment variables

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
 // Start Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
