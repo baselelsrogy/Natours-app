5eb18e94d9c078100e635e7ced0613b489c240fe
baselelsrogy <baselhany88@gmail.com>
2025-09-23 01:22:28 +0300

    add aggregation for tour stats and monthly plan

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -5,9 +5,9 @@ const tourController = require(`./../controllers/tourController`);
 const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
-// Handling GET request, Handling POST request => CREATE
+router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
-// GET tour by id, Handling PATCH Request => update, Handling DELETE Request
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 module.exports = router;
