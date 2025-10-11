e45bb8ce5419937a42796f5f63c67f0b695e6e0f
baselelsrogy <baselhany88@gmail.com>
2025-10-11 15:14:15 +0300

    build tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
+    tour,
+    locations: JSON.stringify(tour.locations),
   });
 };
--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     lowecase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  phtot: String,
+  photo: String,
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
