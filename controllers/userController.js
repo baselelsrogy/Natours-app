671262e41e04a6c0d052db716c66e2c8516a81ff
baselelsrogy <baselhany88@gmail.com>
2025-10-25 22:00:28 +0300

    saving image name to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,6 +52,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 2) update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
